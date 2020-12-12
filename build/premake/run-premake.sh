f194b6e108699c7b01b1d3afb2d7c31588a78968
Doug Hodson <doug@sidechannel.net>
2020-12-11 19:40:43 -0500

    updated premake files

--- build/premake/run-premake.sh
+++ build/premake/run-premake.sh
@@ -6,10 +6,6 @@ rm -rf ..\vs2017
 rm -rf ..\vs2019
 rm -rf ..\obj
 #
-# remove any compiled libraries
-#
-rm -rf ..\..\lib\*.lib
-#
 # create new build files files
 #
 ./premake5-alpha15 --os=linux --file=premake5.lua gmake
